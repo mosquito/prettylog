15e60919b2785ce9c568b7ef1d786d89f6316e11
Dmitry Orlov <me@mosquito.su>
2018-04-17 17:49:44 +0300

    [fix] bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='prettylog',
-    version='0.1.0',
+    version='0.1.1',
     platforms="all",
     author="Dmitry Orlov",
     author_email="me@mosquito.su",
